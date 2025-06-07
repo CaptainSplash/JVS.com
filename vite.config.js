fb49d92e10b4504e5e8d2ae305926da51de993a1
John Shields <john.shields321@gmail.com>
2025-06-07 18:38:00 +0100

    AA reduced image sizes for test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-export default {
-  plugins: [react()],
-};
+export default defineConfig({
+  plugins: [react(), visualizer()],
+});
